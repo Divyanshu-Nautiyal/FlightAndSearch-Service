68a42263e68dc40b5faac4c4a1263da015451708
Divyanshu Nautiyal <nautiyal4divyanshu@gmail.com>
2026-01-10 17:51:40 +0530

    Added Body-Parser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const {PORT} = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
     const app = express();
-    const bodyParser = require("body-parser");
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
     
     app.listen(PORT, ()=>{
         console.log(`Server is running on port ${PORT}`);
